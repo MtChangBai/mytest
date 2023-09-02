de98be0e0fa37ddfcdb589b0eb804efc25afea0b
MtChangBai <143879353+MtChangBai@users.noreply.github.com>
2023-09-03 00:53:48 +0900

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package mtchangbai.javase;
+
+public class HelloWorld {
+
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++){
+			System.out.println("ddddddd111111111111");
+			System.out.println("dd22222222111");
+		}
+	}
+}
