ab161c92f5bfd1ea6a0fec935a453d25f48ab5b8
MtChangBai <143879353+MtChangBai@users.noreply.github.com>
2023-09-03 00:57:49 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,7 @@ package mtchangbai.javase;
 public class HelloWorld {
 	public static void main(String[] args) {
 		for(int i=0;i<10;i++){
-			System.out.println("ddddddd111111111111");
-			System.out.println("dd22222222111");
+			System.out.println("HelloWorld");
 		}
 	}
 }
